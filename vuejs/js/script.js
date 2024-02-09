24496a55369e3b216ae3347b5450e30eb6fdcc69
code555man <dekdee1887@gmail.com>
2024-02-09 21:14:15 +0700

    add vue

---
+++ vuejs/js/script.js
@@ -0,0 +1,15 @@
+const { createApp, ref } = Vue;
+
+createApp({
+
+data(){
+    return {
+        isFontColorPicked: '#2196F3',
+        isFontColorSelected: '#2196F3',
+    }
+},
+methods:{
+    
+},
+
+}).mount('#app')
